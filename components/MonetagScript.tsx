5f748a43c95efeb2b62eab37befe12fd677e10e0
kofringo <mtidama@gmail.com>
2026-08-09 16:40:53 +0300

    Fix genre sidebar styling and case-insensitive filtering

--- components/MonetagScript.tsx
+++ components/MonetagScript.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function MonetagScript() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.innerHTML = "(function(s){s.dataset.zone='11539571',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))";
+    script.innerHTML = "(function(s){s.dataset.zone='11539586',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))";
     document.head.appendChild(script);
 
     return () => {
